60635f96bf895d1398b33fc481bbb68a57eecd6a
Jessica Bader <jabader@iastate.edu>
2019-07-22 11:18:00 -0400

    Commented and made congressional data work. Needs pairwise distances cited

--- LDMVU/LDMVU_CongressionalVotingRecords.py
+++ LDMVU/LDMVU_CongressionalVotingRecords.py
@@ -1,34 +1,34 @@
+import pandas as pd
 import torch
 import torch.nn as nn
-import sklearn.datasets
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-import pandas as pd
+from matplotlib import colors
+from sklearn.cluster import KMeans
+from sklearn.metrics.cluster import v_measure_score
+
+import random
 start_time = time.time()
 torch.manual_seed(2)
-import random
 random.seed(2)
 
-
-# Hyper paramters
-m, n, divisor = 0, 0, 0  # will reset these later
-
-num_lm = 20  # num landmarks to choose
-batch_size = 60  # size of each batch
-num_batches = 5  # number of batches
-size = (batch_size * num_batches) + num_lm  # total num samples
-linear_dim1 = 10  # hidden layer dimension in NN
-linear_dim2 = 2  # final dimension of NN
-lbda = 10000  # scaling term for variance in loss equation
+lm_epoch = 5000  # number of epochs for the landmarks
+set_size = 435  # size of a set
+batch_size = 70  # size of a batch
+test_size = 135  # number of testing points
+num_lm = 20  # number of landmarks
+num_batches = 4
+size = (batch_size * num_batches) + num_lm  # total number of training points
+lbda = 1000  # scaling term for variance in loss equation
 epoch = 5000  # number of times to train network
-squeeze = 1  # amount to squeeze the shape by
-set_random = False  # random choice of landmarks vs most connected points
-k_start = 4  # how you find landmarks based off of number of nearest neighbors
+k_start = 3  # how you find landmarks based off of number of nearest neighbors
 k_lm = 5  # number of landmarks each landmark has
-k_lm = 3  # number of landmarks each landmark has
-k_other = 3  # number of landmarks each regular points has
+k_other = 5  # number of landmarks each regular points has
+m = 300  # number of data
+n = 16  # number of dimensions
+categories = 2
 
 
 class Net(nn.Module):
@@ -38,14 +38,12 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.f = nn.Linear(16, 10, bias=True)
-        self.f2 = nn.Linear(10, 5, bias=True)
-        self.f3 = nn.Linear(5, 2, bias=True)
+        self.f2 = nn.Linear(10, 2, bias=True)
 
     def encode(self, x):
         m = nn.LeakyReLU()
         x = m(self.f(x))
-        x = m(self.f2(x))
-        x = self.f3(x)
+        x = self.f2(x)
         return x
 
     def decode(self, x):
@@ -62,31 +60,68 @@ class Net(nn.Module):
         return x
 
 
+def original_clean():
+    """
+    Method to clean the data
+    :return: data and labels
+    """
+    dataset = pd.read_csv('Parliment-1984.csv')
+    X = dataset.iloc[:, 1:].values
+    y = dataset.iloc[:, 0].values
+
+    for i in range(0, 434):
+        if y[i] == 'democrat':
+            y[i] = 0
+        elif y[i] == 'republican':
+            y[i] = 1
+    y = y.astype(int)
+
+    for a in range(0, 434):
+        for b in range(0, 16):
+            if 'y' in X[a][b]:
+                X[a][b] = 1
+            elif 'n' in X[a][b]:
+                X[a][b] = 0
+
+    medians = []
+    for x in range(0, 16):
+        acceptable = []
+        for z in range(0, 434):
+            if (X[z][x] == 1) or (X[z][x] == 0):
+                acceptable.append(X[z][x])
+        med = np.median(acceptable)
+        medians.append(int(med))
+
+    for c in range(0, 434):
+        for d in range(0, 16):
+            if (X[c][d] != 1) and (X[c][d] != 0):
+                X[c][d] = medians[d]
+    X = X.astype(float)
+    X = normalize(X)
+    return X, y
+
+
 def normalize(data):
     """
     Function to normalize the data
     :param data: original data
     :return: normalized data
     """
-    global squeeze, m, n
-    for j in range(n):
-        # find the sum for each column
+    row = np.size(data, 0)
+    col = np.size(data, 1)
+    # find the sum for each column
+    for j in range(col):
         col_sum = 0
-        for i in range(m):
-            col_sum += data[i][j]
+        for i in range(row):
+            col_sum = col_sum + data[i][j]
+        col_sum = col_sum / row
         # subtract the mean from each point in the column
-        col_sum /= m
-        for i in range(m):
-            data[i][j] -= col_sum
-    # squeeze the graph
-    init_graph = data.transpose()
-    init_graph[1] = init_graph[1] / squeeze
-    data = init_graph.transpose()
+        for i in range(row):
+            data[i][j] = data[i][j] - col_sum
     return data
 
 
-# load data set, select land marks at random and remove from data set, return a train_loader
-def load_data(size, num_lm):
+def load_data():
     """
     Function to load the data
     :param size: total number of points to get
@@ -95,48 +130,21 @@ def load_data(size, num_lm):
     data organized in graphs, neighborhood graph for the landmarks, original data, original labels,
     neighborhood graphs for non-landmarks
     """
-    global divisor, m, n, batch_size, set_random
+    global batch_size, divisor
     # import data
-    data = pd.read_csv('pol_data.csv', delimiter=',').values[:, :-1]
-    labels = np.asarray([0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1,
-                         0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1,
-                         0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1,
-                         1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1,
-                         1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1,
-                         1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1,
-                         1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0,
-                         0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0,
-                         0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1,
-                         1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1,
-                         1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1,
-                         0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1,
-                         1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0,
-                         1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0])
-    test_data = data[size:, :]
-    test_labels = labels[size:]
-    data = data[:size, :]
-    labels = labels[:size]
-    m = np.size(data, 0)
-    n = np.size(data, 1)
-    # make landmarks, select x random points in the data set
-    land_marks = np.empty((num_lm, n))
-    top_landmarks_idxs = []
-    if set_random:
-        # select x random points in the data set
-        for i in range(num_lm):
-            index = random.randint(0, m - i)
-            land_marks[i] = data[index]
-            data = np.delete(data, index, axis=0)
-            labels = np.delete(labels, index, axis=0)
-    else:
-        # pick the points with the most neighbors
-        N = NearestNeighbors(n_neighbors=k_start).fit(data).kneighbors_graph(data).todense()
-        N = np.array(N)
-        num_connections = N.sum(axis=0).argsort()[::-1]
-        top_landmarks_idxs = num_connections[:num_lm]
-        land_marks = data[top_landmarks_idxs, :]
-        data = np.delete(data, top_landmarks_idxs, axis=0)
+    data, labels = original_clean()
+    test_data = data[300:, :]
+    test_labels = labels[300:]
+    data = data[:300, :]
+    labels = labels[:300]
 
+    # make landmarks with points with most neighbors
+    N = NearestNeighbors(n_neighbors=k_start).fit(data).kneighbors_graph(data).todense()
+    N = np.array(N)
+    num_connections = N.sum(axis=0).argsort()[::-1]
+    top_landmarks_idxs = num_connections[:num_lm]
+    land_marks = data[top_landmarks_idxs, :]
+    data = np.delete(data, top_landmarks_idxs, axis=0)
     # find the nearest landmarks for the landmarks
     landmark_neighbors = NearestNeighbors(n_neighbors=k_lm).fit(land_marks).kneighbors_graph(land_marks).todense()
     # break data into batches
@@ -146,6 +154,7 @@ def load_data(size, num_lm):
     # create the full neighborhood graph for each batch
     for i in range(divisor):
         holder = data[batch_size * i: batch_size * (i + 1)]
+        # find the nearest landmarks for the rest of the points
         holder_graph = NearestNeighbors(n_neighbors=k_other).fit(land_marks).kneighbors_graph(holder).todense()
         for j in range(batch_size):  # copy over the holder graph
             for l in range(num_lm):
@@ -174,8 +183,8 @@ def train_net(epoch, data, net, opti, batch_graph):
     """
     global divisor, batch_size
     for num in range(epoch):
+        # train each batch
         for batch_id in range(divisor):
-            # train each batch
             batch = torch.from_numpy(data[batch_id]).float()
             batch = batch.view(batch_size, -1)
             batch_distances = pairwise_distances(batch)
@@ -189,11 +198,10 @@ def train_net(epoch, data, net, opti, batch_graph):
             # Find the difference between |img_i - img_j|^2 and |output_i - output_j|^2
             nbr_diff = torch.abs((output_distances_masked - batch_distances_masked))
             nbr_distance = nbr_diff.norm()
-            loss = nbr_distance + lbda * (1 / out[:, 0].var() + 1 / out[:, 1].var())  # lmbda*(1/output.var(dim=0)[0] + 1/output.var(dim=0)[1]) #lmbd
+            loss = nbr_distance + lbda * (1 / out[:, 0].var() + 1 / out[:, 1].var())
             opti.zero_grad()
             loss.backward()
             opti.step()
-            print('Epoch: %f, Step: %f, Loss: %.2f' % (num, batch_id + 1, loss.data.cpu().numpy()))
 
 
 def train_lms(epoch, land_marks, net, opti, landmark_neighbors):
@@ -206,7 +214,6 @@ def train_lms(epoch, land_marks, net, opti, landmark_neighbors):
     :param landmark_neighbors: neighborhood graph
     """
     for num in range(epoch):
-        # train with the landmark points
         global lbda
         batch = torch.from_numpy(land_marks).float().view(num_lm, -1)
         batch_distances = pairwise_distances(batch)
@@ -217,11 +224,10 @@ def train_lms(epoch, land_marks, net, opti, landmark_neighbors):
         output_distances_masked = output_distances * neighbor_graph.float()
         nbr_diff = torch.abs((output_distances_masked - batch_distances_masked))
         nbr_distance = nbr_diff.norm()
-        loss = nbr_distance + lbda * (1 / out[:, 0].var() + 1 / out[:, 1].var())  # lmbda*(1/output.var(dim=0)[0] + 1/output.var(dim=0)[1]) #lmbd
+        loss = nbr_distance + lbda * (1 / out[:, 0].var() + 1 / out[:, 1].var())
         opti.zero_grad()
         loss.backward()
         opti.step()
-        print('LM Epoch: %f, Loss: %.2f' % (num, loss.data.cpu().numpy()))
 
 
 def pairwise_distances(x):
@@ -248,19 +254,21 @@ def evaluate(data, net, t, landmarks):
     :param net:
     :param t:
     :param landmarks:
-    :return:
     """
-    nothing, holder = sklearn.datasets.make_swiss_roll(len(t))
-    for i in range(len(t)):
-        if t[i] == 0:
-            holder[i] = 13.71865229
-        else:
-            holder[i] = 7.28209531
     out = net(torch.from_numpy(data).float(), False)
+    print(time.time() - start_time)
+    t = t.astype(float)
     out = out.detach().numpy()
-    # make the graph
-    plt.scatter(out[:, 0], out[:, 1], c=holder, marker='o')
-    plt.show()
+    print('New score metric')
+    print("Accuracy with centroid")
+    print(score(out, t))
+    cmap = colors.ListedColormap(['red','blue'])
+    plt.scatter(out[:, 0], out[:, 1], c=t, cmap=cmap, marker='o')
+    kmeans = KMeans(n_clusters=2)
+    kmeans.fit(out)
+    vmeasure = v_measure_score(t, kmeans.labels_)
+    print("Accuracy with kmeans")
+    print(vmeasure)
 
 
 def run():
@@ -269,15 +277,62 @@ def run():
     """
     global num_lm
     # load the data
-    data_loader, land_marks, labels, data, batch_graph,lmIndex, saveData, saveLabels, landmark_neighbors = load_data(size, num_lm)
+    batch_loader, land_marks, labels, data, batch_graph, lmIndex, test_data, test_labels, landmark_neighbors = load_data()
     net = Net()
     opti = torch.optim.Adam(net.parameters(), weight_decay=1e-3)
-    # train the landmarks first to spread them out
-    train_lms(epoch, land_marks, net, opti, landmark_neighbors)
-    # train the rest of the points to fit them around the landmarks
-    train_net(epoch, data_loader, net, opti, batch_graph)
-    # evaluate the accuracy
-    evaluate(saveData, net, saveLabels, lmIndex)
+    # train the landmarks first
+    train_lms(lm_epoch, land_marks, net, opti, landmark_neighbors)
+    # train the rest of the data points based off the landmarks
+    train_net(epoch, batch_loader, net, opti, batch_graph)
+    # evaluate the accuracy of the model
+    evaluate(test_data, net, test_labels, lmIndex)
 
+
+def score(final_data, labels):
+    """
+    Find the accuracy score for the data
+    :param final_data: transformed data
+    :param labels: data labels
+    :return: accuracy
+    :return: accuracy
+    """
+    m = np.size(final_data, 0)
+    # Final Scoring
+    eval_arr = np.zeros((10, 2))
+    count_arr = np.zeros(10)
+    for i in range(0, m):
+        color_num = int(labels[i])
+        eval_arr[color_num][0] += final_data[i][0]
+        eval_arr[color_num][1] += final_data[i][1]
+        count_arr[color_num] += 1
+    for i in range(0, 10):
+        eval_arr[i][0] = eval_arr[i][0] / count_arr[i]
+        eval_arr[i][1] = eval_arr[i][1] / count_arr[i]
+    count = 0
+    for i in range(m):
+        min_center = 0
+        min_center_dist = (final_data[i][0] - eval_arr[0][0]) ** 2 + (final_data[i][1] - eval_arr[0][1]) ** 2
+        for j in range(1, 10):
+            if ((final_data[i][0] - eval_arr[j][0]) ** 2 + (final_data[i][1] - eval_arr[j][1]) ** 2) < min_center_dist:
+                min_center_dist = (final_data[i][0] - eval_arr[j][0]) ** 2 + (final_data[i][1] - eval_arr[j][1]) ** 2
+                min_center = j
+        if min_center == labels[i]:
+            count += 1
+    return count / m
+
+set_size = 435
+batch_size = 70
+test_size = 135
+num_lm = 20
+size = (batch_size * 4) + num_lm
+lbda = 10000
+lm_epoch = 5000
+epoch = 5000
+k_start = 3
+k_lm = 4
+k_other = 4
+m = 300
+n = 16
+categories = 2
 
 run()
