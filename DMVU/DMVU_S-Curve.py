97db3b7af637ee55b303bfab10b4eb07bf36cb8d
Thalia-Chai-Zhang <43323506+Thalia-Chai-Zhang@users.noreply.github.com>
2019-07-22 10:49:27 -0400

    Other DMVU Datasets

---
+++ DMVU/DMVU_S-Curve.py
@@ -0,0 +1,181 @@
+"""
+@Author: Bader, J, Nelson, D, Chai-Zhang, T
+Created: 22/7/19
+"""
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import sklearn.datasets
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+import matplotlib.pyplot as plt
+torch.manual_seed(2)
+
+"""
+Hyper-Parameters:
+epoch: Number of Epochs for training
+train_batch_size: Size of the training data from Swiss Roll
+lbda: Lambda sparsity coefficient for loss
+squeeze: Coefficient for reducing a specific dimension in normalize(), default to 1 for no change.
+lr: Learning Rate
+"""
+
+
+"""
+Hyper-parameters for decent accuracy:
+epoch = 20000
+train_batch_size = 400
+lbda = 10
+k = 20
+squeeze = 1
+"""
+epoch = 20000
+train_batch_size = 400
+lbda = 10
+k = 20
+squeeze = 1
+
+
+def load_s_data(size):
+    """
+    Loads points from the S-Curve Distrabution and returns them in an array and an array of labels.
+    
+    Input:
+        size: Number of points to request to be generated from the S-Curve Distrabution
+    Output:
+        x: Dataset of points from the S-Curve Distrabution
+        t: Categorization labels for data
+    """
+    x, t = sklearn.datasets.samples_generator.make_s_curve(size, random_state=0)
+    return x, t
+
+
+def normalize(data):
+    """
+    Normalizes data around the origin.
+
+    Input:
+        data: Array-like to be normalized
+    Output:
+        data: Normalized data set
+    """
+    m = np.size(data, 0)
+    n = np.size(data, 1)
+
+    for j in range(n):
+        col_sum = 0
+        for i in range(m):
+            col_sum += data[i][j]
+        col_sum /= m
+        for i in range(m):
+            data[i][j] -= col_sum
+    initGraph = data.transpose()
+    initGraph[1] = initGraph[1] / squeeze
+    data = initGraph.transpose()
+    return data
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.f = nn.Linear(3, 10, bias=True)
+        self.f2 = nn.Linear(10, 2, bias=True)
+
+    def encode(self, x):
+        m = nn.PReLU()
+        x = m(self.f(x))
+        x = self.f2(x)
+        return x
+
+    def decode(self, x):
+        return x
+
+    def forward(self, x, decode):
+        x = self.encode(x)
+        if decode:
+            x = self.decode(x)
+        return x
+
+
+def train_s_dmvu(epoch, data, net, loss_func, opti, t):
+    '''
+    Training the neural net and produces the final lower dimensional representation
+
+    epoch: number of Epochs for training
+    data: tensor dataset for processing
+    net: DMVU neural net
+    opti: Neural Net Optimizer
+    t: Labels for dataset
+    '''
+    for num in range(epoch):
+        global k, lbda
+        data = data.view(data.size(0), -1)
+        out = net(data, False)
+        output_distances = pairwise_distances(out)
+        data_distances = pairwise_distances(data)
+        # Multiply the distances between each pair of points with the neighbor mask
+        output_distances_masked = output_distances * nbr_graph_tensor.float()
+        data_distances_masked = data_distances * nbr_graph_tensor.float()
+        # Find the difference between |img_i - img_j|^2 and |output_i - output_j|^2
+        nbr_diff = torch.abs((output_distances_masked - data_distances_masked))
+        nbr_distance = nbr_diff.norm()
+        loss = nbr_distance + lbda * (1 / out[:, 0].var() + 1 / out[:, 1].var())  # lmbda*(1/output.var(dim=0)[0] + 1/output.var(dim=0)[1]) #lmbd
+        opti.zero_grad()
+        loss.backward()
+        opti.step()
+
+        print('Epoch [{}/{}], Step [{}], Loss: {:.4f}'.format(num + 1, epoch,  len(data), loss.item()))
+        if num == (epoch - 1):
+            out = out.detach().numpy()
+            print('Final Test Loss:')
+            print(loss)
+            # Graphing
+            plt.scatter(out[:, 0], out[:, 1], c=t, marker='.')
+            name = 'loss' + str(loss.item()) + 'k' + str(k) + '.png'
+            plt.title(name)
+            plt.show()
+
+
+def pairwise_distances(x, y=None):
+    '''
+    Input: x is a Nxd matrix
+           y is an optional Mxd matirx
+    Output: dist is a NxM matrix where dist[i,j] is the square norm between x[i,:] and y[j,:]
+            if y is not given then use 'y=x'.
+    i.e. dist[i,j] = ||x[i,:]-y[j,:]||^2
+    '''
+    x_norm = (x ** 2).sum(1).view(-1, 1)
+    if y is not None:
+        y_t = torch.transpose(y, 0, 1)
+        y_norm = (y ** 2).sum(1).view(1, -1)
+    else:
+        y_t = torch.transpose(x, 0, 1)
+        y_norm = x_norm.view(1, -1)
+
+    dist = x_norm + y_norm - 2.0 * torch.mm(x, y_t)
+    # Ensure diagonal is zero if x=y
+    # if y is None:
+    #     dist = dist - torch.diag(dist.diag)
+    return torch.clamp(dist, 0.0, np.inf)
+
+
+def run_swiss():
+    # training data
+    data, t = load_s_data(train_batch_size)
+    data = normalize(data)
+    nbrs = NearestNeighbors(n_neighbors=k, algorithm='auto').fit(data)
+    nbr_graph = nbrs.kneighbors_graph(data).toarray()
+    global nbr_graph_tensor
+    nbr_graph_tensor = torch.tensor(nbr_graph)
+    data = torch.from_numpy(data).float()
+    net = Net()
+    # loss function
+    loss_func = nn.L1Loss()
+    # optimizer
+    opti = torch.optim.Adam(net.parameters(), weight_decay=1e-3)
+    # train net
+    train_s_dmvu(epoch, data, net, loss_func, opti, t)
+
+
+run_swiss()
--- DMVU/DMVU_SwissRoll.py
+++ DMVU/DMVU_SwissRoll.py
@@ -1,8 +1,7 @@
 """
 @Author: Bader, J, Nelson, D, Chai-Zhang, T
-Created: 19/7/19
+Created: 22/7/19
 """
-
 
 import torch
 import torch.nn as nn
@@ -11,8 +10,6 @@ import sklearn.datasets
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
 import matplotlib.pyplot as plt
-import time
-start_time = time.time()
 torch.manual_seed(2)
 
 """
@@ -25,21 +22,29 @@ squeeze: Coefficient for reducing a specific dimension in normalize(), default t
 lr: Learning Rate
 """
 
+"""
+Hyper-Parameters for a decent accuracy:
+
 epoch = 20000
 train_batch_size = 400
 lbda = 10000
+weight_initial = .1
 k = 8
-squeeze = 1
 lr = .01
-
+squeeze = 1
+"""
 
 def load_swiss_data(size):
     """
-    Loads points from the Swiss Roll distrabution and returns them in an array with an array of labels.
-
-    size: Number of points to request to be generated from the Swiss Roll Distrabution.
+    Loads points from the Swiss Roll Distrabution and returns them in an array and an array of labels.
+    
+    Input:
+        size: Number of points to request to be generated from the Swiss Roll Distrabution
+    Output:
+        x: Dataset of points from the Swiss Roll Distrabution
+        t: Categorization labels for data
     """
-    x, t = sklearn.datasets.samples_generator.make_swiss_roll(size, random_state=0) # random_state=0
+    x, t = sklearn.datasets.make_swiss_roll(size)
     return x, t
 
 
@@ -47,9 +52,11 @@ def normalize(data):
     """
     Normalizes data around the origin.
 
-    data: Array-like to be normalized
+    Input:
+        data: Array-like to be normalized
+    Output:
+        data: Normalized data set
     """
-
     m = np.size(data, 0)
     n = np.size(data, 1)
 
@@ -69,13 +76,14 @@ def normalize(data):
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.f = nn.Linear(3, 10, bias=True)
-        self.f2 = nn.Linear(10, 2, bias=True)
+        self.f = nn.Linear(3, 4)
+        self.f2 = nn.Linear(4, 2)
+        # self.f2 = nn.Linear(linear_dim1, 2)
 
     def encode(self, x):
         m = nn.PReLU()
         x = m(self.f(x))
-        x = self.f2(x)
+        x = m(self.f2(x))
         return x
 
     def decode(self, x):
@@ -88,84 +96,136 @@ class Net(nn.Module):
         return x
 
 
-def train_swiss_dmvu(epoch, data, net, opti, t):
+def train_dmvu(epoch, data, net, loss_func, opti, t):
     '''
     Training the neural net and produces the final lower dimensional representation
-
-    epoch: number of Epochs for training
-    data: tensor dataset for processing
-    net: DMVU neural net
-    opti: Neural Net Optimizer
-    t: Labels for dataset
+    input:
+        epoch: number of Epochs for training
+        data: tensor dataset for processing
+        net: DMVU neural net
+        loss_func: Loss function, should be set to DMVULoss
+        opti: Neural Net Optimizer
+        t: Labels for dataset
     '''
-    data_distances = pairwise_distances(data)
-    data_distances_masked = data_distances * nbr_graph_tensor.float()
     for num in range(epoch):
-        global k, lbda
+        global k, lbda, lr
+        data = data.view(data.size(0), -1)
         out = net(data, False)
-        output_distances = pairwise_distances(out)
-        # Multiply the distances between each pair of points with the neighbor mask
-        output_distances_masked = output_distances * nbr_graph_tensor.float()
-        # Find the difference between |img_i - img_j|^2 and |output_i - output_j|^2
-        nbr_diff = torch.abs((output_distances_masked - data_distances_masked))
-
-        nbr_distance = nbr_diff.norm()
-
-        loss = nbr_distance + lbda * (1 / out[:, 0].var() + 1 / out[:, 1].var())  # lmbda*(1/output.var(dim=0)[0] + 1/output.var(dim=0)[1]) #lmbd
+        global train_loss
+        train_loss = loss_func(out, data) # todo pass in only once
         opti.zero_grad()
-        loss.backward()
+        train_loss.backward()
         opti.step()
-
-        print('Epoch [{}/{}], Step [{}], Loss: {:.4f}'.format(num + 1, epoch, len(data), loss.item()))
+        print('Epoch [{}/{}], Step [{}], Loss: {:.4f}'.format(num + 1, epoch,  len(data), train_loss.item()))
         if num == (epoch - 1):
             out = out.detach().numpy()
             print('Final Test Loss:')
-            print(loss)
+            print(train_loss)
             # Graphing
-            plt.scatter(out[:, 0], out[:, 1], c=t, marker='o')
-            name = 'loss' + str(loss.item()) + 'k' + str(k) + '.png'
+            plt.scatter(out[:, 0], out[:, 1], c=t, marker='.')
+            name = 'loss' + str(train_loss.item()) + 'k' + str(k) + 'lr' + str(lr) + 'weight' + str(weight_initial) + '.png'
             plt.title(name)
-            print("--- %s seconds ---" % (time.time() - start_time))
             plt.show()
+            # plt.savefig(name, bbox_inches='tight')
+            # plt.clf()
 
 
-def pairwise_distances(x, y=None):
-    '''
-    Calculates distances across each element to prevent sparsity across only a single dimension.
+def initialize_weights(net, weight_initial):
+    """
+    Sets an initial weight for a given neural net
 
-    Input: x is a Nxd matrix
-           y is an optional Mxd matirx
-    Output: dist is a NxM matrix where dist[i,j] is the square norm between x[i,:] and y[j,:]
-            if y is not given then use 'y=x'.
-    i.e. dist[i,j] = ||x[i,:]-y[j,:]||^2
-    '''
-    x_norm = (x ** 2).sum(1).view(-1, 1) # square every element, sum, resize to list
-    if y is not None:
-        y_t = torch.transpose(y, 0, 1)
-        y_norm = (y ** 2).sum(1).view(1, -1)
-    else:
-        y_t = torch.transpose(x, 0, 1)
-        y_norm = x_norm.view(1, -1)
+    Input:
+        net: The neural net to have it's initial weights sets
+        weight_initial: The value to set the initial weights to
+    """
+    for z in net.modules():
+        if isinstance(z, nn.Linear):
+            z.weight.data.normal_(0, weight_initial)
 
-    dist = x_norm + y_norm - 2.0 * torch.mm(x, y_t)
-    # Ensure diagonal is zero if x=y
-    return torch.clamp(dist, 0.0, np.inf)
 
+def make_neighborhood(data, k):
+    """
+    Calculates the nearest neightbor matrix for the points of a given dataset
+    
+    Input:
+        data: The dataset to have it's neightbor matrix calculated.
+        k: How many nearest neightbors each point in the matrix has.
+    Output:
+        Nearest Neighbor Matrix of data with k nearest neighbors
+    """
+    neighbors = NearestNeighbors(n_neighbors=k).fit(data).kneighbors_graph(data).todense()
+    return torch.from_numpy(np.array(neighbors))
+
+
+def find_first_term(data):
+    """
+    Calculates pairwise distances between points in the dataset.
+
+    Input:
+        data: Array-like dataset to have pairwise distances calculated of.
+    Output:
+        first_term: Pairwise distances of data
+    """
+    m_og = data.size()[0]
+    data_gram = torch.mm(data, torch.t(data))
+    data_diag = torch.diag(data_gram)
+    data_diag = torch.diag(data_diag)
+    ones = torch.ones((m_og, m_og))
+    first_term = torch.mm(data_diag, ones)
+    first_term += torch.mm(ones, data_diag)
+    first_term -= 2 * data_gram
+    return first_term
+
+
+class DMVULoss(torch.nn.Module):
+    """
+    Custom loss function for DMVU, designed to incentivize maintaining local distances between neighbors while promoting variance in all other distances. 
+
+    Input:
+        latent_space: The lower dimensional representation of the dataset
+        data: The original dataset
+    Output:
+        The loss value
+    """
+    def __init__(self, neighbors):
+        super(DMVULoss, self).__init__()
+        self.neighbors = neighbors
+
+    def forward(self, latent_space, data):
+        m_ls = latent_space.size()[0]
+        #Finds pairwise distances for both original and lower-dimensional representation of dataset.
+        term_1 = find_first_term(data)
+        term_2 = find_first_term(latent_space)
+        #Calculates distances between neighbors only
+        term_1 = self.neighbors.double() * term_1.double()
+        altered_term_2 = self.neighbors.double() * term_2.double()
+        #Finds difference between neighbor distances in original and lower-dimensional representation of dataset.
+        term_1 = torch.norm(term_1.sub(altered_term_2))
+        latent_ones = torch.ones((m_ls, m_ls)).double()
+        #Calculates distances between all non-neighbors in the lower-dimensional representation of the dataset
+        holder = latent_ones.sub(self.neighbors)
+        term_2 = holder.double() * term_2.double()
+        term_2 = lbda / torch.norm(term_2)
+        return term_1 + term_2
 
 def run_swiss():
     # training data
     data, t = load_swiss_data(train_batch_size)
     data = normalize(data)
-    nbrs = NearestNeighbors(n_neighbors=k, algorithm='auto').fit(data)
-    nbr_graph = nbrs.kneighbors_graph(data).toarray()
-    global nbr_graph_tensor
-    nbr_graph_tensor = torch.tensor(nbr_graph)
     data = torch.from_numpy(data).float()
+    # create net
     net = Net()
+    # initialize weights
+    initialize_weights(net, weight_initial)
+    # make neighbor matrix Q
+    neighbors = make_neighborhood(data, k)
+    first_term = find_first_term(data)
+    # create loss function
+    loss_func = DMVULoss(neighbors)
     # optimizer
-    opti = torch.optim.Adam(net.parameters(), weight_decay=1e-3, lr=lr)
+    opti = torch.optim.Adam(net.parameters(), lr=lr, weight_decay=1e-5)
     # train net
-    train_swiss_dmvu(epoch, data, net, opti, t)
+    train_dmvu(epoch, data, net, loss_func, opti, t)
 
 
 run_swiss()
